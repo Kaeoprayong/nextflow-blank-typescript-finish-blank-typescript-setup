0f5f265a8c60a66eccaa61422ae6cf3d4a829b5c
Kaeoprayong Prasert <Kaeoprayong.p@ktc.co.th>
2023-12-07 11:32:25 +0700

    สร้าง summary เลข 2 ตัว

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 // นำ funtion ใน file lib.ts มาใช้
-sayhi('Jang');
-import sayhi, { adminName } from "./lib";
-import { register } from "./lib";
+//sayhi('Jang');
+//mport sayhi, { adminName } from "./lib";
+//import { register } from "./lib";
 
-register(); //คำสั่งใน import จะเปลี่ยนไปตาม function import
+//register(); //คำสั่งใน import จะเปลี่ยนไปตาม function import
 //  No defualt ใส่ {}
 // import ของที่ exportจากไฟล์ทั่วไป ชื่อต้องตรง และอยู่ใน {} เท่านั้น
 // export มาเปลี่ยนชื่อได้
-console.log(adminName);
+//console.log(adminName);
 
-console.log('Hello, TypeScript with Node.js!');
+//console.log('Hello, TypeScript with Node.js!');
+
+//import ค่า sum plus 2 ตัวในไฟล์ lip
+import { summary } from "./lib";
+console.log(summary(15, 40));
--- src/lib.ts
+++ src/lib.ts
@@ -1,12 +1,18 @@
 // lib.ts ประกาศตัวแป
-export default function sayhi(name:string) { //export  ออกไปใช้ file อื่น
+//export default function sayhi(name: string) { //export  ออกไปใช้ file อื่น
 
-    console.log(`sayhi ${name}!`) //command +/ key ลัด comment =>แทรกตัวแปรใน string   
-}
+    //console.log(`sayhi ${name}!`) //command +/ key ลัด comment =>แทรกตัวแปรใน string   
+//}
 //export เฉยๆ ไม่ต้อง default
-export function register(){
-    console.log('register');
-}
-export let adminName = "แอดมิน"
+//export function register() {
+    //console.log('register');
+//}
+//export let adminName = "แอดมิน"
 //no export function
-let noExport = " No Export"
+//let noExport = " No Export"
+
+//export function รับค่า 2 ตัว / return summary plus
+export function summary(first:number, secound:number){
+return first + secound;
+
+}
